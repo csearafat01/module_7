3e5b05f22f43859ce32a82cc5a90d775d45e4477
csearafat01 <54732088+csearafat01@users.noreply.github.com>
2023-09-23 00:20:49 +0600

    module 7 assignment

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,152 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProductList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Product {
+  final String name;
+  final double price;
+  int quantity;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Product(this.name, this.price, this.quantity);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ProductList extends StatefulWidget {
+  const ProductList({super.key});
+
+  @override
+  _ProductListState createState() => _ProductListState();
+}
+
+class _ProductListState extends State<ProductList> {
+  List<Product> products = [
+    Product("Product 1", 10.0, 0),
+    Product("Product 2", 15.0, 0),
+    Product("Product 3", 20.0, 0),
+    Product("Product 4", 20.0, 0),
+    Product("Product 5", 20.0, 0),
+    Product("Product 6", 20.0, 0),
+    Product("Product 7", 20.0, 0),
+    Product("Product 8", 20.0, 0),
+    Product("Product 9", 20.0, 0),
+    Product("Product 10", 20.0, 0),
+    Product("Product 11", 20.0, 0),
+    Product("Product 12", 20.0, 0),
+    Product("Product 13", 20.0, 0),
+    Product("Product 14", 20.0, 0),
+    Product("Product 15", 20.0, 0),
+    Product("Product 16", 20.0, 0),
+    Product("Product 17", 20.0, 0),
+    Product("Product 18", 20.0, 0),
+
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Center(
+            child: Text("Product List")
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        elevation: 18,
+        backgroundColor: Colors.green,
+        onPressed: (){Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => CartPage(products),
+        ));
+        },
+        child: const Icon(Icons.shopping_cart),
+      ),
+      body: ListView.builder(
+        shrinkWrap: true,
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          final product = products[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text(product.name),
+                  subtitle: Text("\$${product.price.toStringAsFixed(2)}"),
+                  trailing: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        Text('Counts : ${product.quantity.toString()}', style: const TextStyle(
+                          fontSize: 14,
+                        ),),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              product.quantity++;
+                              if (product.quantity == 5) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: const Text("Congratulations!"),
+                                      content: Text("You've bought 5 ${product.name}!"),
+                                      actions: [
+                                        TextButton(
+                                          child: const Text("OK"),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              }
+                            });
+                          },
+
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                            ),
+                          child: const Text('Buy Now'),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
+
+class CartPage extends StatelessWidget {
+  final List<Product> products;
+
+  const CartPage(this.products, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int totalQuantity = products.fold(0, (sum, product) => sum + product.quantity);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+            child: Text("Cart")
+        ),
+      ),
+      body: Center(
+        child: Text("Total Products: $totalQuantity"),
+      ),
+    );
+  }
+}
+
